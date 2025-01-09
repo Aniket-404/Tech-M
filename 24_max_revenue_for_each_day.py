2c9d5c71dab3ce7f9630729cc56775a78a6449c3
Aniket-404 <kamble.aniket.ak47@gmail.com>
2025-01-09 18:22:51 +0530

    changes

--- 24_max_revenue_for_each_day.py
+++ 24_max_revenue_for_each_day.py
@@ -1,9 +1,11 @@
-def calcRevenue(days,product,sales):
-    max_revenue = [max(day) for day in sales]
+#return max revenue for each day
+def calcRevenue(sales):
+    max_revenue = []
+    for day in sales:
+        max_revenue.append(max(day))
+
     return max_revenue
 
 days, product = map(int,input().split(" "))
-sales = [list(map(int,input().split(" "))) for _ in range(days)]
-
-result = calcRevenue(days,product,sales)
-print(" ".join(map(str,result)))
+sales = [list(map(int,input().split())) for i in range(days)]
+print(" ".join(map(str,calcRevenue(sales))))
